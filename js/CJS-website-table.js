5b125afadf3fcc8aae6bc75d477e69def422b9c2
Camden_Shaw <camden.shaw@gmail.com>
2017-08-07 14:36:24 -0700

    first commit of bio-site

---
+++ js/CJS-website-table.js
@@ -0,0 +1,33 @@
+/*This is the Javascript to make my first website look fancy*/
+// creating a variable to track clicks
+var clickCount = 0;
+
+// beginning main function
+function main() {
+    //hiding table then fading it in on load
+    $('.table').hide();
+    $('.table').fadeIn(1000);
+    $('.face').show();
+    // creating consequence for clicking on 'x' above picture
+    $(this).on('click','button', function () {
+        //add 1 to clickCount
+        clickCount = clickCount + 1;
+        //toggle picture off
+        $('#upperLeft').slideToggle(500);
+        $('.face').slideToggle(500);
+        //turn 'x' into 'o'
+        if (clickCount % 2 === 1) {
+            $('.closeButton').text('o');
+        }
+        
+    //  Some Code to make the 'o' return to an 'x' that didn't work.
+        else {
+            $('.closeButton').text('x');
+        }
+    });
+    
+}
+
+
+
+$(document).ready(main);
